2b740bbe0089e6ea088ad46689bac84f9b313f2f
YashanC <YashanC@users.noreply.github.com>
2025-02-22 21:59:14 -0500

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.static('public'));
-
-app.listen(3000, () => console.log("Server running"));
